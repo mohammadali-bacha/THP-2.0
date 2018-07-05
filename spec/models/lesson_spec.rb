097fe85aa1a3166a9eeed2736cb0d9dfd15098ff
MOHAMMAD-ALI <bacha@tuta.io>
2018-07-05 21:16:49 +0200

    add validations and rspec

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -12,5 +12,14 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
++  it 'is create_table' do
++    lesson = create(:lesson)
++    first_lesson = Lesson.first
++    expect(first_lesson.title).to eq(lesson.title)
++    expect(first_lesson.description).to eq(lesson.description)
+end
+  it { should validate_presence_of(:title) }
+  it { should validate_length_of(:title) }.is_at_most(50) }
+  it { should validate_length_of(:description) }.is_at_most(300) }
+
 end
