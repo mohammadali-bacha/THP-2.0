af0d673effa874bf5fb2a48110b75efbaa113a14
MOHAMMAD-ALI <bacha@tuta.io>
2018-07-06 13:53:28 +0200

    gemset

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -19,6 +19,6 @@ RSpec.describe Lesson, type: :model do
     expect(first_lesson.description).to eq(lesson.description)
   end
   it { should validate_presence_of(:title) }
-  it { should validate_length_of(:title) }.is_at_most(50)
-  it { should validate_length_of(:description) }.is_at_most(300)
+  it { should validate_length_of(:title).is_at_most(50) }
+  it { should validate_length_of(:description).is_at_most(30) }
 end
