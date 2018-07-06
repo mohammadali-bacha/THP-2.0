a74edbe5ce2163a33bc01c827ecefd34d83cc025
MOHAMMAD-ALI <bacha@tuta.io>
2018-07-06 13:20:46 +0200

    gemset

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -9,17 +9,16 @@
 #  updated_at  :datetime         not null
 #
 
-require 'rails_helper'
+# require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-+  it 'is create_table' do
-+    lesson = create(:lesson)
-+    first_lesson = Lesson.first
-+    expect(first_lesson.title).to eq(lesson.title)
-+    expect(first_lesson.description).to eq(lesson.description)
-end
+  it 'is create_table' do
+    lesson = create(:lesson)
+    first_lesson = Lesson.first
+    expect(first_lesson.title).to eq(lesson.title)
+    expect(first_lesson.description).to eq(lesson.description)
+  end
   it { should validate_presence_of(:title) }
-  it { should validate_length_of(:title) }.is_at_most(50) }
-  it { should validate_length_of(:description) }.is_at_most(300) }
-
+  it { should validate_length_of(:title) }.is_at_most(50)
+  it { should validate_length_of(:description) }.is_at_most(300)
 end
