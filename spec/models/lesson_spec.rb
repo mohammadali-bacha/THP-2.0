f98d565c88e687d51e8b8ad117401a5b7a2f6a4b
MOHAMMAD-ALI <bacha@tuta.io>
2018-07-06 07:10:31 +0200

    rubocp specy

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -12,7 +12,7 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  it "is creatable" do
+  it "is create_table" do
     lesson = create(:lesson)
     expect(lesson.title).not_to be_blank
     expect(lesson.description).not_to be_blank
