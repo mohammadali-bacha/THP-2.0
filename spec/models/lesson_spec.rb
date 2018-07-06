ba39241af4e4c4f7d0c5f19006ae5a8a4df9f249
MOHAMMAD-ALI <bacha@tuta.io>
2018-07-06 13:54:24 +0200

    gemset

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -20,5 +20,5 @@ RSpec.describe Lesson, type: :model do
   end
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title).is_at_most(50) }
-  it { should validate_length_of(:description).is_at_most(30) }
+  it { should validate_length_of(:description).is_at_most(300) }
 end
